b6925b44be1f64eba3a14e8ad68ffa639899a013
nicholas <nja8161995@gmail.com>
2022-08-09 14:15:07 -0500

    Update: Can now draw on etch-a-sketch.
    This is done from the blackCell function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,27 @@
 const container = document.getElementById("container");
 
-
-for (let i = 0; i < 16; i++) {
-    const col = document.createElement("div");
-    container.appendChild(col);
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        col.appendChild(cell);
-    }
+window.onload = function() {
+    createGrid(16,16);
 }
+
+function createGrid(cellRows, cellCols) {
+    for (let i = 0; i < cellCols; i++) {
+        const col = document.createElement("div");
+        container.appendChild(col);
+        for (let j = 0; j <  cellRows; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            col.appendChild(cell);
+        }
+    }
+    blackCell();
+}
+
+function blackCell() {
+    const blackCells = document.querySelectorAll(".cell");
+    blackCells.forEach(cells => cells.addEventListener("mouseover", () => {
+        cells.setAttribute("style", "background: black;");
+    }))
+}
+
+
