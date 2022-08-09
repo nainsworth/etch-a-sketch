f30e355d482b26d813037a90814af732e9cc3d21
nicholas <nja8161995@gmail.com>
2022-08-09 14:16:29 -0500

    Updated: Added comments to code

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 const container = document.getElementById("container");
 
+// Generates Grid on Window startup
 window.onload = function() {
     createGrid(16,16);
 }
 
+// Function that generates cells from user input
 function createGrid(cellRows, cellCols) {
     for (let i = 0; i < cellCols; i++) {
         const col = document.createElement("div");
@@ -17,6 +19,7 @@ function createGrid(cellRows, cellCols) {
     blackCell();
 }
 
+// Function that changes cells to black on mouse over
 function blackCell() {
     const blackCells = document.querySelectorAll(".cell");
     blackCells.forEach(cells => cells.addEventListener("mouseover", () => {
