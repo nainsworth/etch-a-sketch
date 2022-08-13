e24a636c06aa34b11a40efb331fee4f4ea66efb4
nainsworth <nja8161995@gmail.com>
2022-08-13 11:05:09 -0500

    Update:
    Grid will update number of pixels from slider
    
    Slider now displays instant pixel value update
    
    Add a Toggle Grid Button (code now attached to button yet)

--- javascript.js
+++ javascript.js
@@ -4,10 +4,17 @@
  }
 // Default color
  let color = "black";
+ 
+ const gridSlider = document.getElementById("gridSlider");
+ const sliderValue = document.getElementById("sliderValue");
+
+ gridSlider.onmousemove = (e) => changeSliderValue(e.target.value);
 
  // Function that generates cells from user input
  function createGrid(cells) {
     let sketchPad = document.querySelector(".sketchPad");
+    let cellGrid = sketchPad.querySelectorAll("div");
+    cellGrid.forEach((element) => element.remove());
     sketchPad.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
     sketchPad.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
     
@@ -28,4 +35,9 @@ function colorCell() {
 // Function that changes the color to color in
 function changeColor(choice) {
     color = choice;
+}
+
+// Updates Slider Value
+function changeSliderValue(sizeValue) {
+    sliderValue.innerText = `${sizeValue} x ${sizeValue}`;
 }
